8839f3be58aabaf7226bfaf4fda7eb185462c4f7
david_tojo <david_tojo@hotmail.es>
2024-05-21 18:51:41 +0200

    feat: :sparkles: Add navigation

--- src/components/Movies/Card/Card.tsx
+++ src/components/Movies/Card/Card.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
-import { Movie } from "@/models/movie.model";
+import { MovieListItem } from "@/models/movie.model";
 import styles from "./Card.module.css";
+import Link from "next/link";
+import { getImageUrlW780 } from "@/services/movies.service";
 
 interface CardProps {
-	movie: Movie;
+	movie: MovieListItem;
 }
 
 export default function Card({ movie }: CardProps) {
-	const poster = process.env.IMG_URL + movie.poster_path;
+	const poster = getImageUrlW780(movie.poster_path);
 
 	// Redondear hasta 1 decimal
 	const roundRating = (rating: number) => {
@@ -15,11 +17,13 @@ export default function Card({ movie }: CardProps) {
 	};
 
 	return (
-		<div className={styles.card} style={{ backgroundImage: `url(${poster})` }}>
-			<span className={styles.rating}>{roundRating(movie.vote_average)}</span>
-			<div className={styles.overlay}>
-				<h2 className={styles.title}>{movie.title}</h2>
+		<Link href={`/${movie.id}`}>
+			<div className={styles.card} style={{ backgroundImage: `url(${poster})` }}>
+				<span className={styles.rating}>{roundRating(movie.vote_average)}</span>
+				<div className={styles.overlay}>
+					<h2 className={styles.title}>{movie.title}</h2>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 }

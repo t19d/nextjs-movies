fd93779d4c1746287df38850463dfa13bdc6cdd9
david_tojo <david_tojo@hotmail.es>
2024-05-27 15:56:13 +0200

    feat: :sparkles: Add og info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,33 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Buscador de películas",
-  description: "Buscador de películas utilizando Next.js y TMDB API.",
+	title: "Buscador de películas",
+	description: "Buscador de películas utilizando Next.js y TMDB API.",
+	openGraph: {
+		type: "website",
+		locale: "es_ES",
+		url: "https://nextjs-movies-one-mu.vercel.app/",
+		title: "Buscador de películas",
+		description: "Buscador de películas utilizando Next.js y TMDB API.",
+		images: [
+			{
+				url: "https://nextjs-movies-one-mu.vercel.app/img/og.png",
+				width: 1200,
+				height: 630,
+				alt: "Buscador de películas",
+			},
+		],
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="es">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="es">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
